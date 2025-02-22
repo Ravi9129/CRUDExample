1b829e5280a1a4cd95ce7e04052a865cdcd778ce
Ravi Rajput <rrajput12340@gmail.com>
2025-02-23 04:08:19 +0530

    "ravi"

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -20,9 +20,21 @@ namespace CRUDExample.Controllers
 
         [Route("persons/index")]
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index(string SearchBy, string? searchString)
         {
-           List<PersonResponse> persons= _personsService.GetAllPersons();
+            ViewBag.SearchFields = new Dictionary<string, string>()
+            {
+                { nameof(PersonResponse.PersonName), "Person Name" },
+                { nameof(PersonResponse.Email), "Email" },
+                { nameof(PersonResponse.DateOfBirth), "Date of Birth" },
+                { nameof(PersonResponse.Gender), "Gender" },
+                { nameof(PersonResponse.CountryID), "Country" },
+                { nameof(PersonResponse.Address), "Address" },
+
+            };
+           List<PersonResponse> persons= _personsService.GetFilteredPersons(SearchBy, searchString);
+            ViewBag.CurrentSearchBy = SearchBy;
+            ViewBag.CurrentSearchString = searchString;
             return View(persons); // Views/Persons/Index.cshtml
 
         }
